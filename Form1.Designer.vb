8f7b4abd93baefa9964b25b594f9ac613fe0859d
whcmcaloon <colin@whitehinge.com>
2016-07-09 16:55:52 +0100

    Adding a new program
    This shoulda been here ages ago

---
+++ Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ResetOrderForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.OrderNumTB = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ResetToNew = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'OrderNumTB
+        '
+        Me.OrderNumTB.Location = New System.Drawing.Point(12, 25)
+        Me.OrderNumTB.Name = "OrderNumTB"
+        Me.OrderNumTB.Size = New System.Drawing.Size(100, 20)
+        Me.OrderNumTB.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Order Number:"
+        '
+        'ResetToNew
+        '
+        Me.ResetToNew.Location = New System.Drawing.Point(118, 23)
+        Me.ResetToNew.Name = "ResetToNew"
+        Me.ResetToNew.Size = New System.Drawing.Size(121, 23)
+        Me.ResetToNew.TabIndex = 0
+        Me.ResetToNew.Text = "Reset Order To New"
+        Me.ResetToNew.UseVisualStyleBackColor = True
+        '
+        'ResetOrderForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(257, 59)
+        Me.Controls.Add(Me.ResetToNew)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.OrderNumTB)
+        Me.Name = "ResetOrderForm"
+        Me.Text = "Order Reset"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents OrderNumTB As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ResetToNew As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,69 @@
+﻿Imports WHLClasses
+Imports WHLClasses.Linnworks.Orders
+Imports WHLClasses.Orders
+
+Public Class ResetOrderForm
+    Dim Loader As New GenericDataController
+    Private Sub ResetToNew_Click(sender As Object, e As EventArgs) Handles ResetToNew.Click 'Click button / Hit enter, either or.
+        Dim SelectedOrder As ExtendedOrder 'Let's get our order
+        Try
+            SelectedOrder = Loader.LoadOrdex("T:\AppData\Orders\" + OrderNumTB.Text + ".ordex") 'Load it
+            'Confirm the user wants to reset this item.
+            If MsgBox("Order number " + OrderNumTB.Text + " has been found. Its current state is " + SelectedOrder.Status.ToString + ". Are you sure you want to reset this order to _New?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+                SelectedOrder.SetStatus(OrderStatus._New) 'Set it to new
+                SelectedOrder.issues.Clear()
+                Loader.SaveDataToFile(OrderNumTB.Text + ".ordex", SelectedOrder, "T:\AppData\Orders") 'Framework seems to add the \ between the path and item automatically.
+                MsgBox("Order " + OrderNumTB.Text + " has been reset")
+                OrderNumTB.Clear() 'Clear the text box, whatever it is, we're done with it here.
+            End If
+        Catch ex As System.IO.FileNotFoundException 'File not found handling
+            MsgBox("That file doesn't exist.")
+        Catch ex2 As System.ArgumentException 'Someone typed $£%^&*()_ into the box. How silly.
+            MsgBox("File names are only composed of 7 consecutive numbers.")
+        End Try
+        'Do not clear the box yet, it could have been a mistype.
+        OrderNumTB.Focus() 'Focus box.
+    End Sub
+
+    Private Sub OrderNumTB_KeyPressed(sender As Object, e As KeyEventArgs) Handles OrderNumTB.KeyDown 'Pressing enter? Trigger the above function.
+        If e.KeyCode = Keys.Enter Then
+            ResetToNew_Click(Nothing, Nothing)
+        End If
+    End Sub
+
+    Private Sub CheckTrayFiles(OrderNumber As String)
+        Dim trayToReturn As String = ""
+        Dim orderRemovedFromTrays As Boolean = False
+        'Load .TRS file
+        Dim traylist As Trays = Loader.LoadAnything("T:\Appdata\Trays\.trs").Value
+        'Check .TRS data for Order
+        For Each miniTray As MiniTrayData In traylist
+            For Each orderInMiniTray As String In miniTray.OrderIds
+                'If it's found
+                If OrderNumber = orderInMiniTray Then
+                    'get the tray number.
+                    trayToReturn = miniTray.Code
+                    'Load the .OTR
+                    Dim loadedTray As Tray = Loader.LoadTray("T:\Appdata\Trays\" + trayToReturn + ".otr")
+                    'Remove order data from .otr and .trs
+                    orderInMiniTray = Nothing
+                    For Each orderInTray As TrayOrder In loadedTray.Orders
+                        If OrderNumber = orderInTray.TruncatedOrderData.OrderId Then
+                            orderInTray = Nothing
+                        End If
+                    Next
+                    'Save .otr and .trs
+                    Loader.SaveDataToFile(".otr", loadedTray, "T:\Appdata\Trays")
+                    Loader.SaveDataToFile(".trs", traylist, "T:\Appdata\Trays")
+                    orderRemovedFromTrays = True
+                End If
+            Next
+        Next
+
+        If orderRemovedFromTrays Then
+            MsgBox("Removed order from tray " + trayToReturn.Replace("TR", "") + ".")
+        Else
+            MsgBox("Could not find any trays with this order.")
+        End If
+    End Sub
+End Class
